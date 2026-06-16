521104119b6119e45f98c8741ae570aa75ed5ded
jinyung <jinyungisnot@gmail.com>
2026-06-16 15:05:11 +0800

    adjust webpack

--- webpack.common.js
+++ webpack.common.js
@@ -1,13 +1,26 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: "./js/index.js",
+    index: "./js/index.js",
     dashboard: "./js/dashboard.js",
   },
   output: {
     path: path.resolve(__dirname, "dist"),
     clean: true,
-    filename: "[name].js",
+    filename: "[name].bundle.js",
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      filename: "index.html",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      template: "./dashboard.html",
+      filename: "dashboard.html",
+      chunks: ["dashboard"],
+    }),
+  ],
 };
